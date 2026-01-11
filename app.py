a807e43e0222d650227d1ac0e083ca60e8bb41e4
Hailing Fang <hailing.fang@outlook.com>
2026-01-11 13:21:44 +0800

    new progress

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
+#!/usr/bin/env python3
+
+import os
+import uuid
 import flask
 
 app = flask.Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return flask.render_template("index.html")
+    if flask.request.method == "GET":
+        return flask.render_template("index.html")
+    elif flask.request.method == "POST":
+        pass
+    else:
+        pass
 
 
-@app.route("/login.html")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return flask.render_template("login.html")
+    if flask.request.method == "GET":
+        return flask.render_template("login.html")
+    elif flask.request.method == "POST":
+        return flask.render_template("login-successfully.html")
 
-
-@app.route("/register.html")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return flask.render_template("register.html")
+    if flask.request.method == "GET":
+        return flask.render_template("register.html")
+    elif flask.request.method == "POST":
+        userid = flask.request.form["userid"]
+        if not os.path.exists(f"users/{userid}"):
+            os.mkdir(f"users/{userid}")
+        file_uploaded = flask.request.files["avatar"]
+        file_uploaded.save(f"users/{userid}/avatar.png")
+        return flask.render_template("register-successfully.html")
+
+@app.route("/users/<userid>", methods=["GET"])
+def avatar(userid):
+    return flask.send_from_directory(f"users/{userid}/", flask.request.args.get("avatar", ""))
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port="5000", debug=True)
